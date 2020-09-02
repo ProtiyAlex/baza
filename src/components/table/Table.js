cd56cb1424890218294fbb05f66443bc2cecf1fc
Khramov Alex <protiyalex@gmail.com>
2020-09-02 23:31:43 +0300

    start listener

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -3,6 +3,12 @@ import { createTable } from "./table.tamplate";
 
 export class Table extends ExcelComponent {
   static className = "excel__table";
+
+  constructor($root) {
+    super($root, {
+      // listeners: ['click', 'mousedown', 'mousemove', 'mouseup']
+    });
+  }
   toHTML() {
     return createTable(10); //передаем кол-во строк
   }
